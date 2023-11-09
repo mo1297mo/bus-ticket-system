b6743f2d51e276005642347a80cd0ee71fffe43e
Ferdous Rahsepar <rahsepar19@gmail.com>
2023-11-09 13:18:40 +0100

    test restored

--- frontend/src/components/BookingForm.jsx
+++ frontend/src/components/BookingForm.jsx
@@ -14,7 +14,7 @@ function BookingForm() {
     const [isLoading, setIsLoading] = useState(false);
 
     // Base URL setup
-    axios.defaults.baseURL = "https://backend-y2f7.onrender.com";
+    axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 
     useEffect(() => {
         // Fetch the routes
