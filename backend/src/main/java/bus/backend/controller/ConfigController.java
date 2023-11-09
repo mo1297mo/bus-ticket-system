de8b3c159ae9e0a877364c0680a12e29464f65f0
Ferdous Rahsepar <rahsepar19@gmail.com>
2023-11-09 12:32:12 +0100

    test3

--- backend/src/main/java/bus/backend/controller/ConfigController.java
+++ backend/src/main/java/bus/backend/controller/ConfigController.java
@@ -1,2 +1,26 @@
-package bus.backend.controller;public class ConfigController {
+package bus.backend.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class ConfigController {
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    // Endpoint to return CORS allowed origins
+    @GetMapping("/config/cors")
+    public Map<String, String[]> getCorsConfig() {
+        Map<String, String[]> config = new HashMap<>();
+        config.put("allowedOrigins", allowedOrigins);
+        return config;
+    }
 }
+
+
